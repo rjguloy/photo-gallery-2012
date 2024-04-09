f4c64dd41eed86292b1572a5a1f87f333d0886cc
rjguloy <robertjohnguloy@gmail.com>
2024-04-09 19:18:58 +0800

    Added SQL DB inititialization

---
+++ sql_init/photo_gallery_script.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE db_gallery;
+USE db_gallery;
+CREATE TABLE tbl_images(
+id int auto_increment not null primary key, 
+image_filename varchar(255) not null, 
+image_type varchar(50) not null, 
+image_title varchar(60),
+image_description varchar(255));
