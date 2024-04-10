f6bc944c20c60f0735117ec80e53e5b24d52c9b8
rjguloy <robertjohnguloy@gmail.com>
2024-04-10 19:27:22 +0800

    Updated config file with remote db connection

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -2,13 +2,13 @@
 //<------start contents of config.inc.php
 
 //Database Constants
-define ('DB_HOST', ($_SERVER['SERVER_NAME'] === 'localhost' ? 'localhost' : 'localhost'));
-define ('DB_USER', ($_SERVER['SERVER_NAME'] === 'localhost' ? 'root' : 'some_server_username'));
-define ('DB_PASS', ($_SERVER['SERVER_NAME'] === 'localhost' ? '' : 'some_server_password'));
-define ('DB_NAME', ($_SERVER['SERVER_NAME'] === 'localhost' ? 'db_gallery': 'some_server_db'));
+define ('DB_HOST', ($_SERVER['SERVER_NAME'] === 'localhost' ? 'localhost' : ${{ secrets.DB_HOSTNAME }}));
+define ('DB_USER', ($_SERVER['SERVER_NAME'] === 'localhost' ? 'root' : ${{ secrets.DB_USER }}));
+define ('DB_PASS', ($_SERVER['SERVER_NAME'] === 'localhost' ? '' : ${{ secrets.DB_PASS }}));
+define ('DB_NAME', ($_SERVER['SERVER_NAME'] === 'localhost' ? 'db_gallery': 'db_gallery'));
 
 //Website URL Configs
-define ('WEBSITE_PATH', 'http://localhost/');
+define ('WEBSITE_PATH', $_SERVER['HTTP_HOST'] /*'http://localhost/'*/);
 define ('ABSOLUTE_PATH', getcwd() . '/');
 //Get the webpage filename
 define ('PAGE_FILE', basename($_SERVER['PHP_SELF']));
