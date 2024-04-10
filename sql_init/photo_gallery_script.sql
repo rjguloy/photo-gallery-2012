bdac5c4086c96f84595ac7860efddf700440d514
rjguloy <robertjohnguloy@gmail.com>
2024-04-10 19:11:01 +0800

    Modified the initial sql run file

--- sql_init/photo_gallery_script.sql
+++ sql_init/photo_gallery_script.sql
@@ -1,5 +1,9 @@
+DROP DATABASE db_gallery;
+
 CREATE DATABASE db_gallery;
+
 USE db_gallery;
+
 CREATE TABLE tbl_images(
 id int auto_increment not null primary key, 
 image_filename varchar(255) not null, 
